fe54f9fc0f40b49c6e3c87e5a8af9636ee1b9dd6
fadelyking <fadely195@gmail.com>
2024-04-28 11:39:27 +0300

    set active text to bold

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,68 @@
+"use client";
 import Link from "next/link";
 import styles from "./header.module.css";
+import { usePathname } from "next/navigation";
 
 function Header() {
+    const pathname = usePathname();
     return (
         <div className={styles.header}>
             <div className={styles.logo}>
-                <Link href="/">Logo Image</Link>
+                <Link href="/" className={styles.logo}>
+                    Logo Image
+                </Link>
             </div>
             <div className={styles.links}>
-                <Link href="/services" className="link">
+                <Link
+                    href="/services"
+                    className={`${
+                        pathname === "/services"
+                            ? styles.activeLink
+                            : styles.link
+                    }`}
+                >
                     Services
                 </Link>
-                <Link href="/our-team" className="link">
+                <Link
+                    href="/our-team"
+                    className={`${
+                        pathname === "/our-team"
+                            ? styles.activeLink
+                            : styles.link
+                    }`}
+                >
                     Our Team
                 </Link>
-                <Link href="/about" className="link">
-                    Abount
+                <Link
+                    href="/about"
+                    className={`${
+                        pathname === "/about" ? styles.activeLink : styles.link
+                    }`}
+                >
+                    About
                 </Link>
-                <Link href="/blog" className="link">
+                <Link
+                    href="/blog"
+                    className={`${
+                        pathname === "/blog" ? styles.activeLink : styles.link
+                    }`}
+                >
                     Blog
                 </Link>
-                <Link href="/contact" className="link">
+                <Link
+                    href="/contact"
+                    className={`${
+                        pathname === "/contact"
+                            ? styles.activeLink
+                            : styles.link
+                    }`}
+                >
                     Contact
                 </Link>
             </div>
             <div>
-                <Link href="/book" className="link">
-                    Book Your Appointment
+                <Link href="/book" className={styles.button}>
+                    Book
                 </Link>
             </div>
         </div>
